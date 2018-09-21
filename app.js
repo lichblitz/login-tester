f6ac23f20cf0d0da5ef873161eebfd942c37a363
Carlos Cortes <ccortesp@everis.com>
2018-09-21 11:48:24 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.post('/login', function (req, res) {
 
 })
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
