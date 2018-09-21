2ea860d6617658a0944bf938874a8060564f08e9
Carlos Cortes <ccortesp@everis.com>
2018-09-21 11:26:33 -0500

    upgrade to app.js with openshift recomendations

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.post('/login', function (req, res) {
 
 })
 
-app.listen(3000, "0.0.0.0", function () {
-  console.log('Example app listening on port 3000!');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
